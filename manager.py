886e5be7dee590e4550562fa54cb6f6af9c41baa
lihaoABC <lihao0035@gmail.com>
2018-06-06 11:00:11 +0800

    创建相关对象

--- manager.py
+++ manager.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+
+
 from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+CSRFProtect(app)
+Session(app)
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
@@ -13,4 +26,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
