c77229e4079e60c21ed79999c6fb7667319b122e
lihaoABC <lihao0035@gmail.com>
2018-06-06 11:13:02 +0800

    添加日志

--- manager.py
+++ manager.py
@@ -2,12 +2,13 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from config import DevelopmentConfig, ProductionConfig
-from info import create_app, db
+from info import create_app, db, set_logs
 
 app = create_app(DevelopmentConfig)
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+set_logs(app)
 
 
 @app.route('/')
