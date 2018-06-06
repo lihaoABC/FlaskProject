834b3a3f45eec975dc32363a61917528653d16e4
lihaoABC <lihao0035@gmail.com>
2018-06-06 10:54:53 +0800

    添加项目基本配置信息

--- config.py
+++ config.py
@@ -1,2 +1,34 @@
+import logging
+
+
 class Config(object):
-    pass
+    """working config"""
+
+    # 密钥
+    SECRET_KEY = 'cjfCZ2FT7/ROFRBkV95qup9hspojKEXFxWy/lC+lms0='
+
+    # 默认日志记录等级
+    LOG_LEVEL = logging.DEBUG
+
+    # SQLAlchemy数据库配置信息
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/com_2'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis缓存数据库配置信息
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    # session同步redis配置
+    SESSION_TYPE = 'redis'
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400
+
+
+class DevelopmentConfig(Config):
+    """ 开发者模式"""
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """产品上线模式"""
+    LOG_LEVEL = logging.ERROR
