ce730f5f75abc78f3533fc87ec9fd0557a150d9e
lihaoABC <lihao0035@gmail.com>
2018-06-06 14:59:41 +0800

    注册跟路由，显示网站小图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,14 @@
 from info.modules.index import index_blue
+from flask import render_template, current_app
 
 
-@index_blue.route('/index')
+@index_blue.route('/')
 def index():
 
-    return ''
+    return render_template('news/index.html')
+
+
+@index_blue.route('/favicon.ico')
+def get_ico():
+
+    return current_app.send_static_file('news/favicon.ico')
